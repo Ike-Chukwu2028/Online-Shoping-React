63d276ebc7e071f3d5ac78855323dc04fb0958ce
Ike-Chukwu2028 <ikechukwuemmanuel2028@gmail.com>
2023-09-11 15:30:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 class connectionExample extends React.Component {
   constructor() {
@@ -8,22 +9,44 @@ class connectionExample extends React.Component {
     };
   }
 
-  fetchData(){
-    fetch('127.0.0.1:8000/api/')
-    .then(respons=>respons.json())
-    .then((data)=>{
+  fetchData() {
+  fetch('http://127.0.0.1:8000/api')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
       this.setState({
-        data:data
+        data: data,
       });
-      console.log(data)
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
     });
-  }
+}
+
 
   componentDidMount(){
     this.fetchData();
   }
   render() {
-    return <div>Example connection</div>;
+    return <div>
+      <header>Data Generated From Django</header>
+      <hr></hr>
+      {this.state.data.map((output, id) => (
+        <div key={id}>
+          <div>
+            <h2>{output.name}</h2>
+            <img src={output.image.url} alt={output.name}/>
+            <h3>{output.description}</h3>
+            <h3>${output.price}</h3>
+          </div>
+        </div>
+      ))}
+    </div>
   }
 }
 export default connectionExample;
